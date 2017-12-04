4f92788a7f53538e366f9806ae51d8ffeaf32260
Steve Jeong and Tiffany Kim <pair+optyyie+yoonyoungkim89@gmail.com>
2017-12-04 11:35:50 +0900

    timezone test

--- index.js
+++ index.js
@@ -137,8 +137,8 @@ agenda.on('ready', function () {
         }
 
         // batch
-        agenda.every('25 11 * * *', 'get interview infos for notification'); // cron 표현식 : '분 시 일 월 요일'
-        agenda.every('34 11 * * *', 'start to send notification');
+        agenda.every('37 11 * * *', 'get interview infos for notification'); // cron 표현식 : '분 시 일 월 요일'
+        agenda.every('40 11 * * *', 'start to send notification');
 
         // test
         // agenda.every('30 seconds', 'get interview infos for notification'); // cron 표현식 : '분 시 일 월 요일'
