256e69d794bfa6e9695e276bbd8452ab4bdcf24e
Jake Kim and Yena Kim <pair+copyx00+yenarue@gmail.com>
2018-05-11 12:19:39 +0900

    개발 단계별 config 설정 추가
    NODE_ENV 정규화를 위함
    - staging, production 레벨 추가

--- config.js
+++ config.js
@@ -1,33 +1,37 @@
-const config = {
-    development: {
-        agendaDBUrl: process.env.AGENDA_MONGO_URL,
-        dbUrl: process.env.MONGO_URL,
-        firebaseMessaging: {
-            serverKey: process.env.SERVER_KEY
-        },
-        backup: {
-            host: process.env.BACKUP_HOST,
-            port: '27017',
-            username: process.env.BACKUP_USERNAME,
-            password: process.env.BACKUP_PASSWORD,
-            dbName: 'appbee',
-            outputPath: process.env.BACKUP_OUTPUT_PATH ,
-        }
+const config = {};
+
+config.development = {
+    agendaDBUrl: process.env.AGENDA_MONGO_URL,
+    dbUrl: process.env.MONGO_URL,
+    firebaseMessaging: {
+        serverKey: process.env.SERVER_KEY
     },
-    test: {
-        agendaDBUrl: process.env.AGENDA_MONGO_URL,
-        dbUrl: process.env.MONGO_URL,
-        firebaseMessaging: {
-            serverKey: 'testServerKey'
-        },
-        backup: {
-            host: process.env.BACKUP_HOST,
-            port: '27017',
-            username: process.env.BACKUP_USERNAME,
-            password: process.env.BACKUP_PASSWORD,
-            dbName: 'test',
-            outputPath: process.env.BACKUP_OUTPUT_PATH + 'test/',
-        }
+    backup: {
+        host: process.env.BACKUP_HOST,
+        port: '27017',
+        username: process.env.BACKUP_USERNAME,
+        password: process.env.BACKUP_PASSWORD,
+        dbName: 'appbee',
+        outputPath: process.env.BACKUP_OUTPUT_PATH ,
+    }
+};
+
+config.staging = config.development;
+config.production = config.development;
+
+config.test = {
+    agendaDBUrl: process.env.AGENDA_MONGO_URL,
+    dbUrl: process.env.MONGO_URL,
+    firebaseMessaging: {
+        serverKey: 'testServerKey'
+    },
+    backup: {
+        host: process.env.BACKUP_HOST,
+        port: '27017',
+        username: process.env.BACKUP_USERNAME,
+        password: process.env.BACKUP_PASSWORD,
+        dbName: 'test',
+        outputPath: process.env.BACKUP_OUTPUT_PATH + 'test/',
     }
 };
 
