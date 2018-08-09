7a926e45e360baeec3f1bd6c93929d7e04c703ab
yenarue <yenarue@gmail.com>
2018-08-09 20:11:00 +0900

    style: backup시 log에 tag 추가

--- jobs/backupShortTermStats.js
+++ jobs/backupShortTermStats.js
@@ -1,16 +1,18 @@
 const shell = require('shelljs');
 const config = require('../config');
 
+const tag = '[backupShortTermStats]';
+
 const backup = (downloadFilePath) => {
     renameCommand();
     downloadCommand(downloadFilePath);
     dropCommand();
     moveToAwsS3(downloadFilePath);
-    console.log('============ backup done ============');
+    console.log(tag, '============ backup done ============');
 };
 
 const renameCommand = () => {
-    console.log('================ 1. rename from short-term-stats to backup-short-term-stats');
+    console.log(tag, '================ 1. rename from short-term-stats to backup-short-term-stats');
     const response = shell.exec('mongo --host=' + config.backup.host + ' --port=' + config.backup.port +
         ' -u ' + config.backup.username + ' -p ' + config.backup.password + ' --authenticationDatabase admin' +
         ' --eval "db.getCollection(\'short-term-stats\').renameCollection(\'backup-short-term-stats\')"' +
@@ -19,15 +21,15 @@ const renameCommand = () => {
 
     const regex = /[^{]*{[^}]*"?ok"?\s?:\s?([^\s]),?[^}]*}.*/;
     const matchedGroups = response.stdout.match(regex);
-    console.log(matchedGroups);
+    console.log(tag, matchedGroups);
 
     if (matchedGroups === null || matchedGroups === undefined || matchedGroups.length <= 1 || matchedGroups[1] == 0) {
-        console.log('[error] 컬렉션명 변경에 실패했습니다. (backup-short-term-stats가 이미 존재하거나 응답 포맷이 변경되었을 수 있습니다.)');
+        console.log(tag, '[error] 컬렉션명 변경에 실패했습니다. (backup-short-term-stats가 이미 존재하거나 응답 포맷이 변경되었을 수 있습니다.)');
     }
 };
 
 const downloadCommand = (downloadFilePath) => {
-    console.log('================ 2. mongoexport');
+    console.log(tag, '================ 2. mongoexport');
     const response = shell.exec('mongoexport --host=' + config.backup.host + ' --port=' + config.backup.port +
         ' -u ' + config.backup.username + ' -p ' + config.backup.password + ' --authenticationDatabase admin' +
         ' --ssl --sslAllowInvalidCertificates --db=' + config.backup.dbName +
@@ -36,7 +38,7 @@ const downloadCommand = (downloadFilePath) => {
 };
 
 const dropCommand = () => {
-    console.log('================ 3. drop backup-short-term-stats');
+    console.log(tag, '================ 3. drop backup-short-term-stats');
     const response = shell.exec('mongo --host=' + config.backup.host + ' --port=' + config.backup.port +
         ' -u ' + config.backup.username + ' -p ' + config.backup.password + ' --authenticationDatabase admin' +
         ' --eval "db.getCollection(\'backup-short-term-stats\').drop()"' +
@@ -46,31 +48,33 @@ const dropCommand = () => {
 
     const regex = /.*(true).*/;
     const matchedGroups = response.stdout.match(regex);
-    console.log(matchedGroups);
+    console.log(tag, matchedGroups);
 
     if (matchedGroups === null || matchedGroups === undefined || matchedGroups.length <= 1 || matchedGroups[1] !== 'true') {
-        console.log('[error] 삭제에 실패했습니다. (backup-short-term-stats이 존재하지 않거나 응답 포맷이 변경되었을 수 있습니다.)');
+        console.log(tag, '[error] 삭제에 실패했습니다. (backup-short-term-stats이 존재하지 않거나 응답 포맷이 변경되었을 수 있습니다.)');
     }
 };
 
 const checkResponse = (response) => {
-    console.log(response);
+    console.log(tag, response);
 
     if (response.code !== 0) {
-        console.log('[error] previous command occur a error. so, this job is going to finish');
+        console.log(tag, '[error] previous command occur a error. so, this job is going to finish');
         console.error(response.stderr);
+
+        // TODO : 논의필요. 배치는 무정지 서버로 유지해야하지 않을지?
         shell.exit(1);
     }
 };
 
 const moveToAwsS3 = (downloadFilePath) => {
-    console.log('================ 4. compress and move to aws s3');
-    console.log('================ 4.1 compress');
+    console.log(tag, '================ 4. compress and move to aws s3');
+    console.log(tag, '================ 4.1 compress');
     const gzFilePath = downloadFilePath + '.gz';
     const compressResponse = shell.exec('gzip ' + downloadFilePath);
 
     if(compressResponse.code === 0) {
-        console.log('================ 4.2 move to aws s3');
+        console.log(tag, '================ 4.2 move to aws s3');
         shell.exec('aws s3 mv ' + gzFilePath + ' s3://short-term-stats');
     }
 };
