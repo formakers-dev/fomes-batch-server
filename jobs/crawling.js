04ff9d5cb51944b57bf3ce08e56db278dd889047
Jake Kim <copyx00@gmail.com>
2018-12-18 18:03:17 +0900

    refactor: config 모듈에 uncrawledApps 크롤러 환경변수 추가 및 실행명령에 적용
    https://trello.com/c/D8xGPRCL

--- jobs/crawling.js
+++ jobs/crawling.js
@@ -1,21 +1,23 @@
 const shell = require('shelljs');
+const config = require('../config');
 
-const tag = '[crawling]';
+const tag = () => {
+    return '[' + new Date().toISOString() + '][crawler]';
+};
 
 const runCrawlerForUncrawledApps = () => {
-    console.log(tag, 'Execute shell commands to run crawler for UncrawledApps');
+    console.log(tag(), 'Execute shell commands to run crawler for UncrawledApps');
 
-    const response = shell.exec('cd ${FOMES_CRAWLER_DIR_PATH} &&' +
-        ' nohup scrapy crawl ${FOMES_UNCRAWLED_APP_SPIDER_NAME} > ${FOMES_CRAWLER_LOG_DIR_PATH}/$(date +%Y-%m-%d_%H:%M)_${FOMES_UNCRAWLED_APP_SPIDER_NAME}.log 2> ${FOMES_CRAWLER_LOG_DIR_PATH}/$(date +%Y-%m-%d_%H:%M)_${FOMES_UNCRAWLED_APP_SPIDER_NAME}.err &');
+    const response = shell.exec(`cd ${config.crawler.rootDirPath} && nohup scrapy crawl ${config.crawler.uncrawledApp.spiderName} > ${config.crawler.uncrawledApp.getLogFilePath()} 2> ${config.crawler.uncrawledApp.getErrorLogFilePath()} &`);
 
     checkResponse(response);
 };
 
 const checkResponse = (response) => {
-    console.log(tag, response);
+    console.log(tag(), response);
 
     if (response.code !== 0) {
-        console.log(tag, '[error] previous command occur a error. so, this job is going to finish');
+        console.log(tag(), '[error] previous command occur a error. so, this job is going to finish');
         console.error(response.stderr);
 
         // TODO : 논의필요. 배치는 무정지 서버로 유지해야하지 않을지?
